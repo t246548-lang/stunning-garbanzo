5b3b6d51eea96b167af19aee8f55c7f982e91053
t246548-lang <t246548@st.yamagata-u.ac.jp>
2025-10-10 19:14:03 +0900

    Add files via upload

---
+++ souseikiso2kadai.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{//"日本語を使えるのは""のなかだけ"//
+//"数字を付けて保存"//
+    int goma = 2525;
+    //"書き換え可能"//
+    int mannto;
+    mannto = goma +2;
+    //"intタイプの数字を%dで呼び出す、\nで改行"//
+    printf("はじめまして %d\n",mannto);
+    printf("こんにちは　%d",goma);
+//";{}をわすれない"//
+    return 0;
+}
